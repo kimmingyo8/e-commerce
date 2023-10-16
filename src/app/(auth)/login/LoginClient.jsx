d202554a69788116646a2e16fb0eb5657ce1dcbc
kimmingyo8 <kimmingyo8@naver.com>
2023-10-17 04:04:35 +0900

    Divider 작업

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -9,6 +9,7 @@ import styles from './Auth.module.scss';
 import Loader from '@/components/loader/Loader';
 import Input from '@/components/input/Input';
 import AutoSignInCheckbox from '@/components/autoSignInCheckbox/AutoSignInCheckbox';
+import Divider from '@/components/divider/Divider';
 
 const LoginClient = () => {
   const [email, setEmail] = useState('');
@@ -71,6 +72,9 @@ const LoginClient = () => {
             <div className={styles.buttonGroup}>
               {/* Button */}
               Button
+              <Divider />
+              Button
+              <Divider />
               <div>
                 {/* Button */}
                 Button
