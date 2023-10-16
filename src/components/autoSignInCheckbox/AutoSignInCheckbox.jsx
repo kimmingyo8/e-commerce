92792badd8c9076896564547879c87fc3b798bd0
kimmingyo8 <kimmingyo8@naver.com>
2023-10-17 03:18:38 +0900

    자동로그인 ui 및 체크박스 작업

--- src/components/autoSignInCheckbox/AutoSignInCheckbox.jsx
+++ src/components/autoSignInCheckbox/AutoSignInCheckbox.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import styles from './AutoSignInCheckbox.module.scss';
+import Checkbox from '../checkbox/Checkbox';
+import Tooltip from '../tooltip/Tooltip';
 
-const AutoSignInCheckbox = () => {
-  return <div>AutoSignInCheckbox</div>;
+const AutoSignInCheckbox = ({
+  label = '자동 로그인',
+  checked,
+  disabled,
+  orientation = 'top',
+  message = '개인 정보 보호를 위해 본인 기기에서만 이용해주세요.',
+  onChange,
+  ...restProps
+}) => {
+  return (
+    <span className={styles.formControl}>
+      <Checkbox
+        label={label}
+        checked={checked}
+        disabled={disabled}
+        onChange={onChange}
+        {...restProps}
+      />
+      {checked && <Tooltip />}
+    </span>
+  );
 };
 
 export default AutoSignInCheckbox;
