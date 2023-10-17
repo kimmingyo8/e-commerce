035a675661decb9759997e16b8b0da20948afe3e
kimmingyo8 <kimmingyo8@naver.com>
2023-10-18 06:07:37 +0900

    firebase auth 회원가입 및 유효성 검사 추가

--- src/app/(auth)/register/RegisterClient.jsx
+++ src/app/(auth)/register/RegisterClient.jsx
@@ -10,17 +10,73 @@ import Link from 'next/link';
 import Button from '@/components/button/Button';
 import Divider from '@/components/divider/Divider';
 import LogoPath from '@/assets/colorful.svg';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '@/firebase/firebase';
+import { toast } from 'react-toastify';
 const RegisterClient = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [cPassword, setCPassword] = useState('');
+  const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
 
+  const onChangeInputValidate = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'email') {
+      setEmail(value);
+      const validRegex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+      if (!validRegex.test(value)) {
+        setError('이메일 형식이 올바르지 않습니다.');
+      } else {
+        setError('');
+      }
+    }
+    if (name === 'password') {
+      setPassword(value);
+      if (value?.length < 8) {
+        setError('비밀번호는 8자리 이상 입력해주세요.');
+      } else if (value !== passwordConfirm) {
+        setError('비밀번호와 비밀번호 확인 값이 다릅니다.');
+      } else {
+        setError('');
+      }
+    }
+    if (name === 'cPassword') {
+      setCPassword(value);
+      if (value?.length < 8) {
+        setError('비밀번호는 8자리 이상 입력해주세요.');
+      } else if (value !== password) {
+        setError('비밀번호와 비밀번호 확인 값이 다릅니다.');
+      } else {
+        setError('');
+      }
+    }
+  };
+
   const registerUser = (e) => {
     e.preventDefault();
+    if (password !== cPassword) {
+      return toast.error('비밀번호가 일치하지 않습니다.');
+    }
     setIsLoading(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        console.log(userCredential);
+
+        toast.success('회원가입이 완료되었습니다.');
+        router.push('/login');
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        toast.error(err.message);
+      });
   };
 
   return (
@@ -42,7 +98,7 @@ const RegisterClient = () => {
               placeholder="아이디(이메일)"
               className={styles.control}
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={onChangeInputValidate}
             />
             <Input
               password
@@ -53,20 +109,22 @@ const RegisterClient = () => {
               placeholder="비밀번호"
               className={styles.control}
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={onChangeInputValidate}
             />
 
             <Input
               password
               icon="lock"
-              id="password"
-              name="password"
+              id="cPassword"
+              name="cPassword"
               label="비밀먼호 확인"
               placeholder="비밀번호 확인"
               className={styles.control}
               value={cPassword}
-              onChange={(e) => setCPassword(e.target.value)}
+              onChange={onChangeInputValidate}
             />
+
+            {error && <p className={styles.validateError}>{error}</p>}
 
             <div className={styles.buttonGroup}>
               {/* Button */}
