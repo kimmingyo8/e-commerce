d81b93b912fa511972a03fe738d5c68498df3a5d
kimmingyo8 <kimmingyo8@naver.com>
2023-10-19 16:47:44 +0900

    슬라이더 UI 작업

---
+++ src/components/slider/SliderData.js
@@ -0,0 +1,22 @@
+const sliderData = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1696972646493-c2838682a5cc?auto=format&fit=crop&q=80&w=1315&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    heading: 'title 1',
+    desc: 'description 1',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1677725284611-b15f2bedcf60?auto=format&fit=crop&q=80&w=1287&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    heading: 'title 2',
+    desc: 'description 2',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1685731891348-ccf2702c1ead?auto=format&fit=crop&q=80&w=1336&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    heading: 'title 3',
+    desc: 'description 3',
+  },
+];
+
+export default sliderData;
