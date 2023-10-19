8d9a6450b7cc58f492379e847afb938efa8ff544
kimmingyo8 <kimmingyo8@naver.com>
2023-10-20 05:27:12 +0900

    redux로 filter 값 관리 작업

---
+++ src/redux/slice/filterSlice.js
@@ -0,0 +1,72 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const initialState = {
+  filteredProducts: [],
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    FILTER_BY_CATEGORY: (state, action) => {
+      const { products, category } = action.payload;
+      let tempProduct = [];
+      if (category === 'All') {
+        tempProduct = products;
+      } else {
+        tempProduct = products.filter(
+          (product) => product.category === category,
+        );
+      }
+      state.filteredProducts = tempProduct;
+    },
+    FILTER_BY_BRAND: (state, action) => {
+      const { products, brand } = action.payload;
+      let tempProduct = [];
+      if (brand === 'All') {
+        tempProduct = products;
+      } else {
+        tempProduct = products.filter((product) => product.brand === brand);
+      }
+      state.filteredProducts = tempProduct;
+    },
+    FILTER_BY_PRICE: (state, action) => {
+      const { products, price } = action.payload;
+      let tempProducts = [];
+
+      tempProducts = products.filter((product) => product.price <= price);
+    },
+
+    FILTER_BY: (state, action) => {
+      const { products, price, brand, category } = action.payload;
+      let tempProducts = [];
+
+      if (category === 'All') {
+        tempProducts = products;
+      } else {
+        tempProducts = products.filter(
+          (product) => product.category === category,
+        );
+      }
+      if (brand === 'All') {
+        tempProducts = products;
+      } else {
+        tempProducts = products.filter((product) => product.brand === brand);
+      }
+
+      tempProducts = tempProducts.filter((product) => product.price <= price);
+      state.filteredProducts = tempProducts;
+    },
+  },
+});
+
+export const {
+  FILTER_BY_BRAND,
+  FILTER_BY_CATEGORY,
+  FILTER_BY_PRICE,
+  FILTER_BY,
+} = filterSlice.actions;
+
+export const selectFilteredProducts = (state) => state.filter.filteredProducts;
+
+export default filterSlice.reducer;
