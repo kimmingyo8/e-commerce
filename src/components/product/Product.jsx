811c1db98601cb3eb4e270b47fffa00e4a49a732
kimmingyo8 <kimmingyo8@naver.com>
2023-10-20 03:43:22 +0900

    product 데이터 redux에서 관리 작업 및 product 관련 컴포넌트 생성

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,18 +1,47 @@
 'use client';
 
 import useFetchCollection from '@/hooks/useFetchCollection';
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Product.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  GET_PRICE_RANGE,
+  STORE_PRODUCTS,
+  selectProducts,
+} from '@/redux/slice/productSlice';
+import Loader from '../loader/Loader';
+import ProductList from './ProductList/ProductList';
+import ProductFilter from './ProductFilter/ProductFilter';
 
 const Product = () => {
   const { data, isLoading } = useFetchCollection('products');
 
-  console.log(data);
+  const dispath = useDispatch();
+
+  useEffect(() => {
+    dispath(
+      STORE_PRODUCTS({
+        products: data,
+      }),
+    );
+
+    dispath(
+      GET_PRICE_RANGE({
+        products: data,
+      }),
+    );
+  }, [data, dispath]);
+
+  const products = useSelector(selectProducts);
 
   return (
     <section className={styles.product}>
-      <aside className={styles.filter}></aside>
-      <div className={styles.content}></div>
+      <aside className={styles.filter}>
+        {isLoading ? null : <ProductFilter />}
+      </aside>
+      <div className={styles.content}>
+        {isLoading ? <Loader /> : <ProductList />}
+      </div>
     </section>
   );
 };
