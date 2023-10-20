ffb75cbb8ae7565e7ccbf8d6d9720a88716404a9
kimmingyo8 <kimmingyo8@naver.com>
2023-10-20 21:23:09 +0900

    상품 아이템 UI 생성 및 next.config에 domain 추가

--- src/components/product/ProductItem/ProductItem.jsx
+++ src/components/product/ProductItem/ProductItem.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import styles from './ProductItem.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import priceFormat from '@/utils/priceFormat';
+import { Rating } from 'react-simple-star-rating';
+import rocketBadgeIcon from '@/assets/badge-rocket.svg';
 
-const ProductItem = () => {
-  return <div>ProductItem</div>;
+const ProductItem = ({ id, name, price, imageURL }) => {
+  const shortenText = (text, n) => {
+    if (text.length > n) {
+      const shortenedText = text.substring(0, n).concat('...');
+      return shortenedText;
+    }
+    return text;
+  };
+  return (
+    <div className={styles.grid}>
+      <Link href={`/product-details/${id}`}>
+        <div className={styles.img}>
+          <Image src={imageURL} alt={name} width={265} height={265} />
+        </div>
+      </Link>
+      <div className={styles.content}>
+        <div className={styles.details}>
+          <p>{shortenText(name, 10)}</p>
+          <em>
+            <strong style={{ color: '#cb1400' }}>{priceFormat(price)}</strong>원{' '}
+            <Image src={rocketBadgeIcon} alt="로켓 배송" />
+          </em>
+          <div className={styles.rating}>
+            <Rating size={17} readonly initialValue={4} />
+            <span className={styles.ratingCount}>(3)</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductItem;
